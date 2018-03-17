a6fcc7b7ffe7cee76a88d3c19168a296a2ba4ea0
Aditya Saligrama <saligrama427@gmail.com>
2018-03-17 19:19:05 -0400

    Added hwloc to benchmarking

--- map_exp/bench_script/run_bench.py
+++ map_exp/bench_script/run_bench.py
@@ -3,7 +3,7 @@
 import os, os.path, subprocess, sys, csv, multiprocessing
 
 def get_rust_time (num_threads, bench_type):
-	command = "perflock cargo run --release -- -m " + bench_type + " -t " + str(num_threads)
+	command = "perflock hwloc-bind --cpubind socket:0.pu:0 cargo run --release -- -m " + bench_type + " -t " + str(num_threads)
 	output = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True, cwd="../rust").communicate()
 	return int(output[0])
 
@@ -12,7 +12,7 @@ def main ():
 	try:
 		writer = csv.writer(f);
 		writer.writerow(("NumThreads", "BenchType", "NumOpsIn10Secs"))
-		for num_threads in range(1, multiprocessing.cpu_count()+1):
+		for num_threads in range(1, 8):
 			for bench_type in ["r", "w", "rw"]:
 				for i in range(3):
 					writer.writerow((num_threads, bench_type, get_rust_time(num_threads, bench_type)))
