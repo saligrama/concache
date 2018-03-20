7ac5e6c3f810f92f8e9b12638cc4b7dad59a902e
Aditya Saligrama <saligrama427@gmail.com>
2018-03-20 13:06:01 -0400

    Added 20thread noHT results

--- map_exp/bench_script/run_bench.py
+++ map_exp/bench_script/run_bench.py
@@ -6,15 +6,15 @@ def get_rust_time (num_threads, bench_type):
         if num_threads > 10:
                 command = "perflock hwloc-bind --cpubind package:0 cargo run --release -- -m " + bench_type + " -t " + str(num_threads)
         else:
-                command = "perflock hwloc-bind --physical --cpubind pu:0-18 ~package:1 cargo run --release -- -m " + bench_type + " -t " + str(num_threads)
+                command = "perflock hwloc-bind --physical --cpubind pu:0-20 cargo run --release -- -m " + bench_type + " -t " + str(num_threads)
         output = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True, cwd="../rust").communicate()
         return int(output[0])
 
 def main ():
-	f = open("../results/rust_mutex_bound_16_hyperthread.csv","wt");
+	f = open("../results/rust_mutex_bound_20_noht.csv","wt");
 	try:
 		writer = csv.writer(f);
-		writer.writerow(("NumThreads", "BenchType", "NumOpsIn10Secs"))
+		writer.writerow(("NumThreads", "BenchType", "NumOpsIn5Secs"))
 		for num_threads in range(1, 21):
 			for bench_type in ["r", "w", "rw"]:
 				for i in range(3):
