b937e4ce24f60eee00550cf1c24a2e4e8318c342
Aditya Saligrama <saligrama427@gmail.com>
2018-03-24 18:35:53 -0400

    Membind results

--- map_exp/bench_script/run_bench.py
+++ map_exp/bench_script/run_bench.py
@@ -3,20 +3,17 @@
 import os, os.path, subprocess, sys, csv, multiprocessing
 
 def get_rust_time (num_threads, bench_type):
-        if num_threads > 10:
-                command = "perflock hwloc-bind --cpubind package:0 cargo run --release -- -l r -m " + bench_type + " -t " + str(num_threads)
-        else:
-                command = "perflock hwloc-bind --physical --cpubind pu:0-18 cargo run --release -- -l r -m " + bench_type + " -t " + str(num_threads)
+        command = "perflock hwloc-bind --cpubind node:0 --membind node:0 cargo run --release -- -l r -m " + bench_type + " -t " + str(num_threads)
         output = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True, cwd="../rust").communicate()
         return int(output[0])
 
 def main ():
-	f = open("../results/rust_rwlock.csv","wt");
+	f = open("../results/rust_rwlock_ro_nobound.csv","wt");
 	try:
 		writer = csv.writer(f);
 		writer.writerow(("NumThreads", "BenchType", "NumOpsIn5Secs"))
-		for num_threads in range(1, 21):
-			for bench_type in ["r", "w", "rw"]:
+		for num_threads in [1, 2, 4, 8, 16]:
+			for bench_type in ["r"]:
 				for i in range(3):
 					writer.writerow((num_threads, bench_type, get_rust_time(num_threads, bench_type)))
 					f.flush()
