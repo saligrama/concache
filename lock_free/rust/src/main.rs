503e685ffc62a02cd44f03ca7c3474b60f80676a
Aditya Saligrama <saligrama427@gmail.com>
2018-06-16 17:53:40 -0400

    Epoch implemented, compiles but panics

--- lock_free/rust/src/main.rs
+++ lock_free/rust/src/main.rs
@@ -5,26 +5,30 @@
 #![feature(integer_atomics)]
 
 extern crate rand;
+extern crate crossbeam;
 
 use std::sync::{Mutex, RwLock, atomic::*, Arc};
 use std::ptr;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
 use std::thread;
-use rand::thread_rng;
 use std::fmt;
+use rand::thread_rng;
+
+use crossbeam::epoch::{self, Atomic, Owned};
 
 const AVG_PER_BIN_THRESH : usize = 4;
 
 struct Node {
     kv: (usize, Mutex<usize>),
     active: AtomicBool,
-    next: AtomicPtr<Node>
+    next: Atomic<Node>,
+    prev: Atomic<Node>
 }
 
 struct LinkedList {
     size: AtomicUsize,
-    first: AtomicPtr<Node>,
+    first: Atomic<Node>,
 }
 
 struct Table {
@@ -43,7 +47,8 @@ impl Node {
         Node {
             kv: (k, Mutex::new(v)),
             active: AtomicBool::new(true),
-            next: AtomicPtr::new(ptr::null_mut())
+            next: Atomic::null(),
+            prev: Atomic::null()
         }
     }
 }
@@ -52,130 +57,248 @@ impl LinkedList {
     fn new () -> Self {
         LinkedList {
             size: AtomicUsize::new(0),
-            first: AtomicPtr::new(ptr::null_mut()),
+            first: Atomic::null(),
         }
     }
 
     fn insert (&self, kv : (usize, usize)) -> bool {
-        let ins = Box::new(Node::new(kv.0, kv.1));
+        let guard = epoch::pin();
 
-        if self.first.load(Ordering::SeqCst).is_null() {
-            // nonexistent first node
-            self.first.compare_and_swap(ptr::null_mut(), Box::into_raw(ins), Ordering::SeqCst);
-        } else {
-            let mut not_mutated = true;
-            let p = Box::into_raw(ins);
+        // if self.first.load(Ordering::SeqCst, &guard).is_null() {
+        //     // nonexistent first node
+        //     self.first.compare_and_swap(ptr::null_mut(), Box::into_raw(ins), Ordering::SeqCst);
+        // } else {
+        //     let mut not_mutated = true;
+        //     let p = Box::into_raw(ins);
+        //
+        //     while not_mutated {
+        //         let mut node_cur : &Node;
+        //         let mut ptr_cur = &self.first;
+        //         let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //
+        //         while !ptr_raw.is_null() {
+        //             node_cur = unsafe { &*ptr_raw };
+        //             ptr_cur = &node_cur.next;
+        //             ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //
+        //             // found same key
+        //             if node_cur.kv.0 == kv.0 && node_cur.active.load(Ordering::SeqCst) == true {
+        //                 let mut change = node_cur.kv.1.lock().unwrap();
+        //                 *change = kv.1;
+        //                 return false;
+        //             }
+        //         }
+        //
+        //         let ret = ptr_cur.compare_and_swap(ptr::null_mut(), p, Ordering::SeqCst);
+        //         if ret == ptr::null_mut() {
+        //             not_mutated = false;
+        //         }
+        //     }
 
-            while not_mutated {
-                let mut node_cur : &Node;
-                let mut ptr_cur = &self.first;
-                let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
-
-                while !ptr_raw.is_null() {
-                    node_cur = unsafe { &*ptr_raw };
-                    ptr_cur = &node_cur.next;
-                    ptr_raw = ptr_cur.load(Ordering::SeqCst);
-
-                    // found same key
-                    if node_cur.kv.0 == kv.0 {
-                        let mut change = node_cur.kv.1.lock().unwrap();
+        let mut node = &self.first;
+        loop {
+            match node.load(Ordering::Relaxed, &guard) {
+                Some(k) => {
+                    let mut raw = k.as_raw();
+                    let mut cur = unsafe { &*raw };
+                    if cur.kv.0 == kv.0 && cur.active.load(Ordering::Relaxed) {
+                        let mut change = cur.kv.1.lock().unwrap();
                         *change = kv.1;
-                        // TODO should I reuse a node or not?????
-                        node_cur.active.store(true, Ordering::SeqCst);
                         return false;
                     }
+                    node = &k.next;
+                },
+                None => {
+                    break;
                 }
+            };
+        }
 
-                let ret = ptr_cur.compare_and_swap(ptr::null_mut(), p, Ordering::SeqCst);
-                if ret == ptr::null_mut() {
-                    not_mutated = false;
-                }
+        // key does not exist
+        let mut ins = Owned::new(Node::new(kv.0, kv.1));
+        loop {
+            let first = self.first.load(Ordering::Relaxed, &guard);
+            ins.next.store_shared(first, Ordering::Relaxed);
+
+            match self.first.cas_and_ref(first, ins, Ordering::Release, &guard) {
+                Ok(_) => break,
+                Err(owned) => ins = owned
             }
         }
 
-        self.size.fetch_add(1, Ordering::SeqCst);
-        true
+        // update the prev reference of first.next to reform the doubly-linked list
+        let first = self.first.load(Ordering::Relaxed, &guard);
+        let k = first.unwrap().as_raw();
+        let k_raw = unsafe { &*k };
+        let next = k_raw.next.load(Ordering::Relaxed, &guard).unwrap();
+        let next_raw = unsafe { &*next.as_raw() };
+        next_raw.prev.store_shared(first, Ordering::Relaxed);
+
+        return true;
     }
 
     fn get (&self, key : usize) -> Option<usize> {
-        if !self.first.load(Ordering::SeqCst).is_null() {
+        // if !self.first.load(Ordering::SeqCst).is_null() {
+        //
+        //     let mut node_cur : &Node;
+        //     let mut ptr_cur = &self.first;
+        //     let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //
+        //     while !ptr_raw.is_null() {
+        //         node_cur = unsafe { &*ptr_raw };
+        //         if node_cur.kv.0 == key {
+        //             let active = node_cur.active.load(Ordering::SeqCst);
+        //             if !active {
+        //                 return None;
+        //             }
+        //             let value = node_cur.kv.1.lock().unwrap();
+        //             return Some(*value);
+        //         }
+        //
+        //         ptr_cur = &node_cur.next;
+        //         ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //     }
+        // }
+        // None
 
-            let mut node_cur : &Node;
-            let mut ptr_cur = &self.first;
-            let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
-
-            while !ptr_raw.is_null() {
-                node_cur = unsafe { &*ptr_raw };
-                if node_cur.kv.0 == key {
-                    let active = node_cur.active.load(Ordering::SeqCst);
-                    if !active {
-                        return None;
+        let guard = epoch::pin();
+        let mut node = &self.first;
+        loop {
+            match node.load(Ordering::Relaxed, &guard) {
+                Some(k) => {
+                    let mut raw = k.as_raw();
+                    let mut cur = unsafe { &*raw };
+                    if cur.kv.0 == key && cur.active.load(Ordering::Relaxed) {
+                        let value = cur.kv.1.lock().unwrap();
+                        return Some(*value);
                     }
-                    let value = node_cur.kv.1.lock().unwrap();
-                    return Some(*value);
+                    node = &k.next;
+                },
+                None => {
+                    return None;
                 }
-
-                ptr_cur = &node_cur.next;
-                ptr_raw = ptr_cur.load(Ordering::SeqCst);
-            }
+            };
         }
-        None
+
     }
 
     fn remove (&self, key : usize) -> bool {
-        if !self.first.load(Ordering::SeqCst).is_null() {
+        // if !self.first.load(Ordering::SeqCst).is_null() {
+        //
+        //     let mut node_cur : &Node;
+        //     let mut ptr_cur = &self.first;
+        //     let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //
+        //     while !ptr_raw.is_null() {
+        //         node_cur = unsafe { &*ptr_raw };
+        //         if node_cur.kv.0 == key {
+        //             if node_cur.active.store(false, Ordering::SeqCst) {
+        //                 return true;
+        //             }
+        //             return false;
+        //         }
+        //
+        //         ptr_cur = &node_cur.next;
+        //         ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //     }
+        // }
+        // false
+        let guard = epoch::pin();
+        let mut node = &self.first;
+        loop {
+            match node.load(Ordering::Relaxed, &guard) {
+                Some(k) => {
+                    let mut raw = k.as_raw();
+                    let mut cur = unsafe { &*raw };
+                    if cur.kv.0 == key && cur.active.load(Ordering::Relaxed) {
+                        cur.active.store(false, Ordering::SeqCst);
 
-            let mut node_cur : &Node;
-            let mut ptr_cur = &self.first;
-            let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+                        let next = k.next.load(Ordering::Relaxed, &guard);
+                        let prev = k.prev.load(Ordering::Relaxed, &guard);
 
-            while !ptr_raw.is_null() {
-                node_cur = unsafe { &*ptr_raw };
-                if node_cur.kv.0 == key {
-                    if node_cur.active.store(false, Ordering::SeqCst) {
-                        return true;
+                        node.cas_shared(Some(k), next, Ordering::Release);
+
+                        let mut new_node = node.load(Ordering::Relaxed, &guard).unwrap();
+                        let mut new_node_raw_cur = unsafe { &*new_node.as_raw() };
+
+                        if new_node_raw_cur.prev.cas_shared(Some(k), prev, Ordering::Release) {
+                            unsafe { guard.unlinked(k) };
+                            return true;
+                        }
                     }
+                    node = &k.next;
+                },
+                None => {
+                    // the node with key key didn't exist
                     return false;
                 }
-
-                ptr_cur = &node_cur.next;
-                ptr_raw = ptr_cur.load(Ordering::SeqCst);
-            }
+            };
         }
-        false
     }
 }
 
 impl fmt::Display for LinkedList {
     fn fmt (&self, f : &mut fmt::Formatter) -> fmt::Result {
+        // let mut ret = String::new();
+        // if !self.first.load(Ordering::Relaxed, &guard).is_null() {
+        //
+        //     let mut node_cur : &Node;
+        //     let mut ptr_cur = &self.first;
+        //     let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //
+        //     while !ptr_raw.is_null() {
+        //         node_cur = unsafe { &*ptr_raw };
+        //         let active = node_cur.active.load(Ordering::SeqCst);
+        //         // let active = true;
+        //         if active {
+        //             let key = node_cur.kv.0;
+        //             println!("Taking lock for value");
+        //             let value = node_cur.kv.1.lock().unwrap();
+        //             println!("Took lock for value");
+        //
+        //             ret.push_str("(");
+        //             ret.push_str(&key.to_string());
+        //             ret.push_str(", ");
+        //             ret.push_str(&value.to_string());
+        //             ret.push_str("), ");
+        //
+        //             println!("Releasing lock for value");
+        //         }
+        //         ptr_cur = &node_cur.next;
+        //         ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //     }
+        // }
+        //
+        // write!(f, "{}", ret)
+
+        let guard = epoch::pin();
         let mut ret = String::new();
-        if !self.first.load(Ordering::SeqCst).is_null() {
+        let mut node = &self.first;
+        loop {
+            match node.load(Ordering::Relaxed, &guard) {
+                Some(k) => {
+                    let mut raw = k.as_raw();
+                    let mut cur = unsafe { &*raw };
+                    if cur.active.load(Ordering::Relaxed) {
+                        let key = cur.kv.0;
+                        println!("Taking lock for value");
+                        let value = cur.kv.1.lock().unwrap();
+                        println!("Took lock for value");
 
-            let mut node_cur : &Node;
-            let mut ptr_cur = &self.first;
-            let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
+                        ret.push_str("(");
+                        ret.push_str(&key.to_string());
+                        ret.push_str(", ");
+                        ret.push_str(&value.to_string());
+                        ret.push_str("), ");
 
-            while !ptr_raw.is_null() {
-                node_cur = unsafe { &*ptr_raw };
-                let active = node_cur.active.load(Ordering::SeqCst);
-                // let active = true;
-                if active {
-                    let key = node_cur.kv.0;
-                    println!("Taking lock for value");
-                    let value = node_cur.kv.1.lock().unwrap();
-                    println!("Took lock for value");
-
-                    ret.push_str("(");
-                    ret.push_str(&key.to_string());
-                    ret.push_str(", ");
-                    ret.push_str(&value.to_string());
-                    ret.push_str("), ");
-
-                    println!("Releasing lock for value");
+                        println!("Releasing lock for value");
+                    }
+                    node = &k.next;
+                },
+                None => {
+                    break;
                 }
-                ptr_cur = &node_cur.next;
-                ptr_raw = ptr_cur.load(Ordering::SeqCst);
-            }
+            };
         }
 
         write!(f, "{}", ret)
@@ -199,25 +322,50 @@ impl Table {
     }
 
     fn resize (&mut self, nbuckets : usize) {
+        // let guard = epoch::pin;
+        // let new = Table::new(nbuckets);
+        // for i in 0..self.bsize {
+        //     let ll = &self.mp[i];
+        //
+        //     let mut ptr_cur = &ll.first;
+        //     let mut ptr_raw = ptr_cur.load(Ordering::Relaxed, &guard);
+        //
+        //     while !ptr_raw.is_null() {
+        //         let mut node_cur = unsafe { &*ptr_raw };
+        //
+        //         new.insert(node_cur.kv.0, *node_cur.kv.1.lock().unwrap());
+        //
+        //         ptr_cur = &node_cur.next;
+        //         ptr_raw = ptr_cur.load(Ordering::SeqCst);
+        //     }
+        // }
+        //
+        // self.mp = new.mp;
+        // self.bsize = nbuckets;
+
+        let guard = epoch::pin();
+
         let new = Table::new(nbuckets);
         for i in 0..self.bsize {
+
             let ll = &self.mp[i];
-
-            let mut ptr_cur = &ll.first;
-            let mut ptr_raw = ptr_cur.load(Ordering::SeqCst);
-
-            while !ptr_raw.is_null() {
-                let mut node_cur = unsafe { &*ptr_raw };
-
-                new.insert(node_cur.kv.0, *node_cur.kv.1.lock().unwrap());
-
-                ptr_cur = &node_cur.next;
-                ptr_raw = ptr_cur.load(Ordering::SeqCst);
+            let mut node = &ll.first;
+            loop {
+                match node.load(Ordering::Relaxed, &guard) {
+                    Some(k) => {
+                        let mut raw = k.as_raw();
+                        let mut cur = unsafe { &*raw };
+                        if cur.active.load(Ordering::Relaxed) {
+                            new.insert(cur.kv.0, *cur.kv.1.lock().unwrap());
+                        }
+                        node = &k.next;
+                    },
+                    None => {
+                        break;
+                    }
+                };
             }
         }
-
-        self.mp = new.mp;
-        self.bsize = nbuckets;
     }
 
     fn insert (&self, key : usize, value : usize) -> bool {
@@ -323,23 +471,23 @@ impl fmt::Display for HashMap {
 }
 
 fn main() {
-	// let mut new_linked_list = LinkedList::new();
-	// println!("{:?}", new_linked_list);
-	// new_linked_list.insert((3, 2));
-	// new_linked_list.insert((3, 4));
-	// new_linked_list.insert((5, 8));
-	// new_linked_list.insert((4, 6));
-	// new_linked_list.insert((1, 8));
-	// new_linked_list.insert((6, 6));
-	// new_linked_list.print();
+    // let mut new_linked_list = LinkedList::new();
+    // println!("{:?}", new_linked_list);
+    // new_linked_list.insert((3, 2));
+    // new_linked_list.insert((3, 4));
+    // new_linked_list.insert((5, 8));
+    // new_linked_list.insert((4, 6));
+    // new_linked_list.insert((1, 8));
+    // new_linked_list.insert((6, 6));
+    // new_linked_list.print();
 
-	// assert_eq!(new_linked_list.get(3).unwrap(), 4);
-	// assert_eq!(new_linked_list.get(5).unwrap(), 8);
-	// assert_eq!(new_linked_list.get(2), None);
+    // assert_eq!(new_linked_list.get(3).unwrap(), 4);
+    // assert_eq!(new_linked_list.get(5).unwrap(), 8);
+    // assert_eq!(new_linked_list.get(2), None);
 
     println!("Started");
     let mut new_HashMap = HashMap::new(); //init with 16 buckets
-	// new_HashMap.mp[0].push((1,2)); //manually push
+    // new_HashMap.mp[0].push((1,2)); //manually push
     //input values
     new_HashMap.insert(1, 1);
     new_HashMap.insert(2, 5);
@@ -364,7 +512,7 @@ fn main() {
     new_HashMap.remove(20);
     println!("{} {}", new_HashMap.to_string(), new_HashMap.size());
 
-	println!("Finished.");
+    println!("Finished.");
 }
 
 #[cfg(test)]
@@ -436,7 +584,7 @@ mod tests {
             }));
         }
         for t in threads {
-        	println!("here");
+            println!("here");
             t.join().unwrap();
         }
     }
